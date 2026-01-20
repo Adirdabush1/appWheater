71817c478275e278bfc87fffce42c845b0042313
Adir Dabush <155654425+Adirdabush1@users.noreply.github.com>
2026-01-20 15:22:49 -0800

    Initial Commit

---
+++ appTests/appTests.swift
@@ -0,0 +1,17 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by rentamac on 1/20/26.
+//
+
+import Testing
+@testable import app
+
+struct appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
